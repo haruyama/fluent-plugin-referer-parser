6b5491a614685f83de66e6ca49e7f0dedb6df95b
Kenji Okimoto <okimoto@clear-code.com>
2017-06-02 13:58:19 +0900

    Remove redundant code

--- test/helper.rb
+++ test/helper.rb
@@ -4,15 +4,3 @@ require 'test/unit'
 $LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 $LOAD_PATH.unshift(__dir__)
 require 'fluent/test'
-unless ENV.key?('VERBOSE')
-  nulllogger = Object.new
-  nulllogger.instance_eval do |obj|
-    def method_missing(method, *args)
-      # pass
-    end
-  end
-  $log = nulllogger
-end
-
-class Test::Unit::TestCase
-end
