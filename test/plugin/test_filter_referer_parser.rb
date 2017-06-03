6bcadea3022298416d25d08c06727fcf47b294b6
HARUYAMA Seigo <haruyama@unixuser.org>
2017-06-03 15:50:51 +0900

    Avoid rubocop warnings

--- test/plugin/test_filter_referer_parser.rb
+++ test/plugin/test_filter_referer_parser.rb
@@ -8,7 +8,7 @@ class RefererParserFilterTest < Test::Unit::TestCase
     key_name referer
     remove_prefix test
     add_prefix merged
-  )
+  ).freeze
 
   CONFIG2 = %(
     key_name ref
@@ -16,14 +16,14 @@ class RefererParserFilterTest < Test::Unit::TestCase
     out_key_referer      ref_referer
     out_key_host         ref_host
     out_key_search_term  ref_search_term
-  )
+  ).freeze
 
   CONFIG3 = %(
     type referer_parser
     key_name ref
     referers_yaml test/data/referers.yaml
     encodings_yaml test/data/encodings.yaml
-  )
+  ).freeze
 
   def setup
     Fluent::Test.setup
@@ -56,7 +56,7 @@ class RefererParserFilterTest < Test::Unit::TestCase
 
     test 'filter & merge' do
       d = create_driver CONFIG2
-      assert_equal 'ref',    d.instance.key_name
+      assert_equal 'ref', d.instance.key_name
 
       assert_equal 'ref_known',       d.instance.out_key_known
       assert_equal 'ref_referer',     d.instance.out_key_referer
@@ -72,7 +72,7 @@ class RefererParserFilterTest < Test::Unit::TestCase
         { 'value' => 2, 'referer' => 'http://www.unixuser.org/' },
         { 'value' => 3, 'referer' => 'http://www.google.co.jp/search?hl=ja&ie=Shift_JIS&c2coff=1&q=%83%7D%83%8B%83%60%83L%83%83%83X%83g%81@%8Aw%8Em%98_%95%B6&lr=' },
         { 'value' => 4, 'referer' => 'http://www.google.co.jp/search?hl=ja&ie=Shift_J&c2coff=1&q=%83%7D%83%8B%83%60%83L%83%83%83X%83g%81@%8Aw%8Em%98_%95%B6&lr=' },
-        { 'value' => 5, 'referer' => 'http://search.yahoo.co.jp/search?p=%E3%81%BB%E3%81%92&aq=-1&oq=&ei=UTF-8&fr=sfp_as&x=wrt' },
+        { 'value' => 5, 'referer' => 'http://search.yahoo.co.jp/search?p=%E3%81%BB%E3%81%92&aq=-1&oq=&ei=UTF-8&fr=sfp_as&x=wrt' }
       ]
       expected = [
         {
@@ -107,7 +107,7 @@ class RefererParserFilterTest < Test::Unit::TestCase
           'referer_known' => true,
           'referer_referer' => 'Google',
           'referer_host' => 'www.google.co.jp',
-          'referer_search_term' => 'マルチキャスト　学士論文'.encode("Shift_JIS").force_encoding("US-ASCII")
+          'referer_search_term' => 'マルチキャスト　学士論文'.encode('Shift_JIS').force_encoding('US-ASCII')
         },
         {
           'value' => 5,
@@ -145,7 +145,7 @@ class RefererParserFilterTest < Test::Unit::TestCase
         {
           'value' => 2,
           'ref' => 'http://www.unixuser.org/',
-          'ref_known' => false,
+          'ref_known' => false
         },
         {
           'value' => 3,
